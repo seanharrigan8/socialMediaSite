584082ba7b55f665db58166fdd5c9b1aa2353608
Sean Harrigan <SeanHarrigan8@gmail.com>
2024-03-19 18:08:37 -0500

    added ignore and env file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config() 
 const connectDB = require('./config/connection');
-const dotenv = require('dotenv');;
+const dotenv = require('dotenv');
 const routes = require('./routes');
 
 
@@ -9,6 +9,7 @@ const routes = require('./routes');
 
 
 const PORT = process.env.PORT || 5000;
+
 
 const app = express();
 
